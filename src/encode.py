b44941c91af25a3941a26f987d94e7c0895f143d
Kevin Cai <75402062+Kev-in123@users.noreply.github.com>
2021-11-10 17:31:01 -0500

    example sentence + shift paramater to function

--- src/encode.py
+++ src/encode.py
@@ -1,12 +1,13 @@
 s = """
-
+the quick brown fox jumps over the lazy dog
 """
 
-def encode(message):
+def encode(message, shift):
   d = {}
   for c in (65, 97):
     for i in range(26):
-      d[chr(i + c)] = chr((i + 13) % 26 + c)
+      d[chr(i + c)] = chr((i + shift) % 26 + c)
   return "".join(d.get(c, c) for c in message)
 
-print(encode(s))
+print(encode(s, 10))
+#dro aesmu lbygx pyh tewzc yfob dro vkji nyq
